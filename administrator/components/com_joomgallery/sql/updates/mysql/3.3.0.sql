7bf6bb0cf8510b9eabef86d1a0745d61c5d21ebe
VitUrzh <vit@urzhumov.info>
2015-04-05 19:58:25 +1000

    Added default limit in mysql

--- administrator/components/com_joomgallery/sql/updates/mysql/3.3.0.sql
+++ administrator/components/com_joomgallery/sql/updates/mysql/3.3.0.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `#__joomgallery_config` ADD `jg_motionminiLimit` int(2) NOT NULL AFTER `jg_motionminiHeight`;
+ALTER TABLE `#__joomgallery_config` ADD `jg_motionminiLimit` int(2) NOT NULL DEFAULT 0 AFTER `jg_motionminiHeight`;
